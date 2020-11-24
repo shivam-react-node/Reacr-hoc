ca5fbc93082903801e6b40ab0a7b6f4d6fbe5144
shivam-react-node <74953198+shivam-react-node@users.noreply.github.com>
2020-11-24 12:51:53 +0530

    react-hoc changes

---
+++ App.js
@@ -0,0 +1,15 @@
+import React,{ Component } from "react";
+//import ClickEvent from "./cickEvent";
+import ComposedBasicHoverExample from "./ComposedBasicHoverExample";
+
+
+class App extends Component{
+  render(){
+    return(
+      <div className="App">
+        <ComposedBasicHoverExample />
+      </div>
+    )
+  }
+}
+export default App
